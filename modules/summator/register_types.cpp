5799b158aa7ad6e23eb2c57ebac6bd3958aae52f
hungrymonkey <boringmonkey@gmail.com>
2017-10-04 09:11:33 -0700

    init commit, static module as provided by http://docs.godotengine.org/en/latest/development/cpp/custom_modules_in_cpp.html

---
+++ modules/summator/register_types.cpp
@@ -0,0 +1,12 @@
+include "register_types.h"
+#include "class_db.h"
+#include "summator.h"
+
+void register_summator_types() {
+
+        ClassDB::register_class<Summator>();
+}
+
+void unregister_summator_types() {
+   //nothing to do here
+}
