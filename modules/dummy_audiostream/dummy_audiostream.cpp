fd4fa7611db88bc691fe995da4090ec472ecb358
hungrymonkey <psuedofinnish@psuedos-MBP.lan>
2017-11-10 09:57:04 -0800

    trying to make a 639hz tone player

--- modules/dummy_audiostream/dummy_audiostream.cpp
+++ modules/dummy_audiostream/dummy_audiostream.cpp
@@ -24,67 +24,24 @@ void AudioStreamPlaybackDummy::seek(float p_time){
 	}
     base->set_position(uint64_t(p_time * base->mix_rate) << MIX_FRAC_BITS);
 }
-void AudioStreamPlaybackDummy::mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames){
+void AudioStreamPlaybackDummy::_mix_internal(AudioFrame *p_buffer, int p_frames){
 	if (!playing) {
 		for (int i = 0; i < p_frames; i++) {
 			p_buffer[i] = AudioFrame(0, 0);
 		}
 		return;
     }
-    int64_t offset = 0;
     zeromem(pcm_buffer, PCM_BUFFER_SIZE);
-    base->gen_tone((uint16_t * )pcm_buffer, p_frames);
-    do_resample<uint16_t, false>((uint16_t * ) pcm_buffer, p_buffer, offset, p_frames, p_frames);
-	
+	uint16_t * buf = (uint16_t * )pcm_buffer;
+    base->gen_tone(buf, p_frames);
+
+	for(int i = 0; p_frames > i; i++){
+		float sample =  float(buf[i])/32767.0;
+		p_buffer[i] = AudioFrame(sample, sample);
+	}	
 }
-template <class Depth, bool is_stereo>
-void AudioStreamPlaybackDummy::do_resample(const Depth *p_src, AudioFrame *p_dst, int64_t &offset, int32_t &increment, uint32_t amount) {
-
-	// this function will be compiled branchless by any decent compiler
-
-	int32_t final, final_r, next, next_r;
-	while (amount--) {
-		int64_t pos = offset >> MIX_FRAC_BITS;
-		if (is_stereo )
-			pos <<= 1;
-
-		final = p_src[pos];
-		if (is_stereo)
-			final_r = p_src[pos + 1];
-		if (sizeof(Depth) == 1) { /* conditions will not exist anymore when compiled! */
-			final <<= 8;
-			if (is_stereo)
-				final_r <<= 8;
-		}
-		if (is_stereo) {
-			next = p_src[pos + 2];
-			next_r = p_src[pos + 3];
-		} else {
-			next = p_src[pos + 1];
-		}
-		if (sizeof(Depth) == 1) {
-			next <<= 8;
-			if (is_stereo)
-				next_r <<= 8;
-		}
-
-		int32_t frac = int64_t(offset & MIX_FRAC_MASK);
-
-		final = final + ((next - final) * frac >> MIX_FRAC_BITS);
-		if (is_stereo)
-			final_r = final_r + ((next_r - final_r) * frac >> MIX_FRAC_BITS);
-	
-
-		if (!is_stereo) {
-			final_r = final; //copy to right channel if stereo
-		}
-
-		p_dst->l = final / 32767.0;
-		p_dst->r = final_r / 32767.0;
-		p_dst++;
-
-		offset += increment;
-	}
+float AudioStreamPlaybackDummy::get_stream_sampling_rate(){
+	return base->mix_rate;
 }
 int AudioStreamPlaybackDummy::get_loop_count() const {
     return 0;
@@ -99,7 +56,7 @@ bool AudioStreamPlaybackDummy::is_playing() const {
     return playing;
 }
 AudioStreamDummy::AudioStreamDummy()
-    : mix_rate(48000), stereo(false), hz(17100) {
+    : mix_rate(48000), stereo(false), hz(639) {
 }
 
 Ref<AudioStreamPlayback> AudioStreamDummy::instance_playback(){
--- modules/dummy_audiostream/dummy_audiostream.h
+++ modules/dummy_audiostream/dummy_audiostream.h
@@ -10,8 +10,8 @@
 
 class AudioStreamDummy;
 
-class AudioStreamPlaybackDummy : public AudioStreamPlayback {
-	GDCLASS(AudioStreamPlaybackDummy, AudioStreamPlayback)
+class AudioStreamPlaybackDummy : public AudioStreamPlaybackResampled {
+	GDCLASS(AudioStreamPlaybackDummy, AudioStreamPlaybackResampled)
 	friend class AudioStreamDummy;
 private:
 	enum{
@@ -25,6 +25,9 @@ private:
 	void * pcm_buffer;
 	Ref<AudioStreamDummy> base;
 	bool playing;
+protected:
+	virtual float get_stream_sampling_rate();
+	virtual void _mix_internal(AudioFrame *p_buffer, int p_frames);
 public:
 	virtual void start(float p_from_pos = 0.0);
 	virtual void stop();
@@ -32,14 +35,10 @@ public:
 	virtual int get_loop_count() const; //times it looped
 	virtual float get_playback_position() const;
 	virtual void seek(float p_time);
-	virtual void mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames);
 	virtual float get_length() const; //if supported, otherwise return 0
 	
 	AudioStreamPlaybackDummy();
 
-private:
-	template <class Depth, bool is_stereo>
-	void do_resample(const Depth *p_src, AudioFrame *p_dst, int64_t &offset, int32_t &increment, uint32_t amount);
 };
 
 class AudioStreamDummy : public AudioStream {
